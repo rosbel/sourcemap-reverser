7937ef1a5a87687d4affbd39470845e3bc224ea0
rosbel <rlsanroman@gmail.com>
2023-04-22 11:38:30 -0500

    Remove comments

--- index.js
+++ index.js
@@ -6,31 +6,23 @@ const { SourceMapConsumer } = require('source-map');
 
 const { program } = require('commander');
 
-// Read the package.json file
 const packageJson = JSON.parse(fs.readFileSync(path.join(__dirname, 'package.json'), 'utf8'));
 
 async function getSourceCode(minifiedCodePath, sourceMapPath, outputPath) {
-  // Read minified code and source map files
   const minifiedCode = fs.readFileSync(minifiedCodePath, 'utf8');
   const sourceMapRaw = fs.readFileSync(sourceMapPath, 'utf8');
   const sourceMap = JSON.parse(sourceMapRaw);
 
-  // Create a SourceMapConsumer instance
   const smc = await new SourceMapConsumer(sourceMap);
 
-  // Initialize an empty object to store the original source files content
   const sourcesContent = {};
 
-  // Iterate through each source file in the source map
   for (const sourcePath of smc.sources) {
-    // Get the source content
     const sourceContent = smc.sourceContentFor(sourcePath);
 
-    // Save the source content to the sourcesContent object
     sourcesContent[sourcePath] = sourceContent;
     }
 
-  // Save the reconstructed original source files to the outputPath
   for (const sourcePath in sourcesContent) {
     const outputFilePath = path.join(outputPath, sourcePath);
     const sourceDir = path.dirname(outputFilePath);
